d152a2b92b6f6561f836571598d56286bf11bf15
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-09-26 20:09:28 -0600

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from "./components/landing/landing.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FunfactsComponent } from './components/funfacts/funfacts.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent, NavbarComponent, FunfactsComponent],
+  imports: [RouterOutlet, LandingComponent, NavbarComponent, FunfactsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
